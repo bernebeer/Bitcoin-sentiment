8407e66858c24fce44eed2b44c5488f6109b136c
Gijs van Berne <gijs@bernebeer.nl>
2017-05-06 17:39:14 +0200

    Streaming test

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 import tweepy
+from tweepy import OAuthHandler
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
+
+from dev_settings import *
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print tweet.text
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+
+class MyListener(StreamListener):
+
+    def on_data(self, data):
+        print(data)
+        try:
+            with open('python.json', 'a') as f:
+                f.write(data)
+                print()
+                return True
+        except BaseException as e:
+            print(str(e))
+        return True
+
+    def on_error(self, status):
+        print(status)
+        return True
+
+twitter_stream = Stream(auth, MyListener())
+twitter_stream.filter(track=['#bitcoin'])
